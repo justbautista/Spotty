2dc4fb48dd349e0de91a4b717604e9cf1219bcf0
justin <justinbautista923@gmail.com>
2021-11-18 11:42:07 -0500

    axios url

--- client/src/helpers/axios.js
+++ client/src/helpers/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://git.heroku.com/song-bird-app-server.git'
+    baseURL: 'https://song-bird-app-server.herokuapp.com'
 })
 
 export default instance;
