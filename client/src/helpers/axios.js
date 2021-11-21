9ff57c8ed1361c8d14972cef4e94febb01223bc0
justin <justinbautista923@gmail.com>
2021-11-20 23:01:23 -0500

    fixed base url again

--- client/src/helpers/axios.js
+++ client/src/helpers/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-base = process.env.REACT_APP_LOCAL || process.env.REACT_APP_SERVER
+const base = process.env.REACT_APP_LOCAL || process.env.REACT_APP_SERVER
 const instance = axios.create({
     baseURL: base
 })
